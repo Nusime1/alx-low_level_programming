4769fd0897d818939c31da8e1ea9e2f031d39079
nuseyba <nusime851@gmail.com>
2023-02-16 11:30:21 -0800

    Hello, World!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-betty 4-puts.c
+#include <stdio.h>
+
+/**
+ * main - prints to string
+ * Description: Print "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
