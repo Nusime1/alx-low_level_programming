cf44ce2ec41ced9e04b11fc3f4e304afd6acb47d
keneilwen <angy.kn@gmail.com>
2023-02-22 04:03:27 +0100

    Update:
    df

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ * Description - n Times table
+ * @n: Integer variable
+ * Return: int
+ */
+
+void print_times_table(int n)
+{
+	int i, j, ans;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		ans = i * j;
+		for (j = 0; j <= n; j++)
+		{
+			_putchar(ans + '0');
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
